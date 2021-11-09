c77094c5a851ffc1dc113c8b3f13319d48ae8d09
Zane Barker <zane.barker@gmail.com>
2021-11-09 12:58:28 +0900

    Now can zoom on a right-click. Also set non-standard port for web-server.

--- static/js/index.js
+++ static/js/index.js
@@ -1,4 +1,14 @@
 
+function handleClick(evt) {
+    switch (evt.button) {
+        case 0:
+            toggleDarkness(evt);
+            break;
+        case 2:
+            activateZoomModal(evt);
+    }
+}
+
 function toggleDarkness(evt) {
     console.log(evt);
     evt.target.classList.toggle("darkened")
@@ -8,8 +18,11 @@ function addClickListeners() {
     let images = document.getElementsByTagName("img");
 
     for (var i=0;i<images.length;i++) {
-        images[i].addEventListener('click', toggleDarkness);
+        images[i].addEventListener('mouseup', handleClick);
     }
+
+    document.getElementById("zoom-modal-background").addEventListener("click", deactivateZoomModal);
+    document.getElementById("zoom-modal-close").addEventListener("click", deactivateZoomModal);
 }
 
 function findPageDimensions() {
@@ -17,6 +30,18 @@ function findPageDimensions() {
     const pageHeight = document.documentElement.scrollHeight;
 
     console.log(pageWidth, pageHeight);
+}
+
+function activateZoomModal(evt) {
+    let zoomModal = document.getElementById("zoom-modal")
+    zoomModal.classList.add("is-active");
+    console.log(evt);
+    document.getElementById("zoom-modal-img").setAttribute("src", evt.originalTarget.src);
+    document.getElementById("zoom-modal-name").innerText = evt.originalTarget.parentNode.nextElementSibling.innerText;
+}
+
+function deactivateZoomModal() {
+    document.getElementById("zoom-modal").classList.remove("is-active");
 }
 
 
